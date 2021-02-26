23fec7fe5e5ccc6588d0df14a2ed26ac01cea494
guilhermicas <sotrabalhosgui@hotmail.com>
2021-02-26 16:44:50 +0000

    Implementacao de varias funcionalidades
    Remocao de redundancia de busca de palavras, se o utilizador ja tiver o ficheiro com as palavras nao precisa de voltar a dar fetch. Criacao de sistema de notificacao de palavra.

--- app.py
+++ app.py
@@ -1,21 +1,43 @@
-
 import random
 import requests
 from bs4 import BeautifulSoup
+import os.path
+from plyer import notification
 
-res = requests.get(
-    "https://1000mostcommonwords.com/1000-most-common-russian-words/")
+"""
+TODO: Exception handling
+Program flow:
+        First check if the word file exists (russian.txt):
+        if doesnt exist fetch the words from the site and create it.
+        If it exists proceed to:
+            fetch a random line and notify the word and the translation
+"""
 
-soup = BeautifulSoup(res.content, 'html.parser')
 
-total = []
-row = []
-for idx, td in enumerate(soup.find_all('td')):
-    txt = td.get_text()
-    row.append(txt)
-    if(idx % 3 == 0):
-        total.append(row)
-        row = []
+def createWordFile():
+    print("words.txt doesn't exists, fetching words")
+    res = requests.get(
+        "https://1000mostcommonwords.com/1000-most-common-russian-words/")
 
-res = [random.choice(total), random.choice(total), random.choice(total)]
-print(res)
+    soup = BeautifulSoup(res.content, 'html.parser')
+
+    with open("words.txt", "w+") as wordFile:
+        for idx, td in enumerate(soup.find_all('td')):
+            if(idx % 3 == 0):
+                wordFile.write(td.get_text() + "\n")
+                continue
+            wordFile.write(td.get_text() + " ")
+
+
+if __name__ == "__main__":
+    # If the word cache doesnt exist, fetch and create file
+    if not os.path.isfile("./words.txt"):
+        createWordFile()
+    # Fetch random line and create array with Russian word, English Translation and word number
+    randomLine = random.choice(open('./words.txt').readlines())
+
+    # Create notification with random line from file
+    notification.notify(title='New Word To Learn!',
+                        message=randomLine, timeout=30)
+
+    # Enjoy
